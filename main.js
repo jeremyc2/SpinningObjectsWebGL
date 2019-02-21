949b99f9d60faaffead48e578b87207851ff0ec7
justin9four <justinchandler@VaderSomedayLater.local>
2019-02-21 15:01:12 -0700

    added example html file

--- main.js
+++ main.js
@@ -50,8 +50,8 @@ function main(){
     });
 
     var loop = function () {
-
-        gl.clearColor(0.0,0.0,0.0,1.0);
+        
+        gl.clearColor(0.0,1.0,0.0,1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         requestAnimationFrame(loop);
