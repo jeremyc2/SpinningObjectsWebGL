d2f5768a38029cece8936a3318409a31afc1d107
justin9four <justinchandler@VaderSomedayLater.local>
2019-03-11 10:59:10 -0600

    minor changes

--- main.js
+++ main.js
@@ -1,65 +1,58 @@
-var gl;
-var frames;
+var gl
+var frames
 
-function initializeWebGL(){
-    var canvas = document.getElementById("gl");
+function initializeWebGL () {
+  var canvas = document.getElementById('gl')
 
-    gl = canvas.getContext("webgl") ||
-         canvas.getContext("experimental-webgl") ||
-         canvas.getContext("moz-webgl") ||
-         canvas.getContext("webket-3d");
+  gl =
+    canvas.getContext('webgl') ||
+    canvas.getContext('experimental-webgl') ||
+    canvas.getContext('moz-webgl') ||
+    canvas.getContext('webket-3d')
 
-    if (gl){
-        var extensions = gl.getSupportedExtensions();
+  if (gl) {
+    var extensions = gl.getSupportedExtensions()
 
-        console.log(gl);
-        console.log(extensions);
+    console.log(gl)
+    console.log(extensions)
 
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
-
-    }else{
-        console.log("WebGL not supported!");
-    }
+    gl.viewportWidth = canvas.width
+    gl.viewportHeight = canvas.height
+  } else {
+    console.log('WebGL not supported!')
+  }
 }
 
-function loadResources(err, callback, resourceFiles){
-    
+function loadResources (err, callback, resourceFiles) {}
+
+function calculateFramesPerSecond (err, callback) {
+  if (err) {
+    callback(err)
+  } else {
+    var startFrames = frames
+    setTimeout(function () {
+      callback(err, frames - startFrames)
+    }, 1000)
+  }
 }
 
-function calculateFramesPerSecond(err, callback){
-    if(err){
-        callback(err);
-    }
-    else{
-        var startFrames = frames;
-        setTimeout(function(){ 
-            callback(err, frames - startFrames); 
-        }, 1000);
-    }
-}
+function main () {
+  initializeWebGL()
 
-function main(){
+  frames = 0
 
-    initializeWebGL();
+  calculateFramesPerSecond(null, function (err, framesPerSecond) {
+    console.log(framesPerSecond + ' Frames Per Second')
+  })
 
-    frames = 0;
+  var loop = function () {
+    gl.clearColor(0.0, 1.0, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    calculateFramesPerSecond(null, function(err, framesPerSecond){
-        console.log(framesPerSecond + " Frames Per Second");
-    });
+    requestAnimationFrame(loop)
 
-    var loop = function () {
-        
-        gl.clearColor(0.0,1.0,0.0,1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    frames++
+  }
 
-        requestAnimationFrame(loop);
-
-        frames++;
-
-    }
-
-    requestAnimationFrame(loop);
-
+  requestAnimationFrame(loop)
 }
