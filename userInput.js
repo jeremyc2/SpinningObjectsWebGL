a5e5b0d29f91482339ee6dd0f116b8bbef1ae578
Jeremy Chandler <goldenmeso@gmail.com>
2020-01-29 21:58:36 -0700

    fixed mobile touches

--- userInput.js
+++ userInput.js
@@ -52,7 +52,7 @@ var dX = 0, dY = 0;
 
 var mouseDown = function(e) {
    drag = true;
-   old_x = e.pageX, old_y = e.pageY;
+   old_x = e.pageX || e.touches[0].pageX, old_y = e.pageY || e.touches[0].pageY;
    e.preventDefault();
    console.log("mouse down");
    return false;
@@ -66,12 +66,13 @@ var mouseUp = function(e){
 var mouseMove = function(e) {
    if (!drag) return false;
    console.log("mouse move");
-   dX = (e.pageX-old_x)*2*Math.PI/canvas.width,
-   dY = (e.pageY-old_y)*2*Math.PI/canvas.height;
+   dX = (e.pageX-old_x || e.touches[0].pageX-old_x)*2*Math.PI/canvas.width,
+   dY = (e.pageY-old_y || e.touches[0].pageY-old_y)*2*Math.PI/canvas.height;
    rotate.y += dX;
    rotate.x += dY;
-   old_x = e.pageX, old_y = e.pageY;
+   old_x = e.pageX || e.touches[0].pageX, old_y = e.pageY || e.touches[0].pageY;
    e.preventDefault();
+   console.log("dX=", dX, " dY=", dY, " e.pageX=", e.touches[0].pageX, " e.pageY=", e.touches[0].pageY);
 };
 
 function rotateControls (e) {
